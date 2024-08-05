fedbafe93685ab4ec904b29f232b97bcaf64e401
18degrees <mikhaili.dev@gmail.com>
2024-08-06 01:20:28 +0300

    perf: make /test static

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,24 +1,20 @@
-"use client"
-
 import { alegreya, shippori_mincho } from '../fonts'
-import { useRouter } from 'next/navigation'
 import style from './page.module.css'
+import Link from 'next/link'
 
 export default function Test() {
-    const router = useRouter()
-
     return (
         <div className={style.container}>
             <h1 className={alegreya.className}>Режимы тестирования</h1>
             <div>
-                <div 
+                <Link
+                    href={'/test/known_writing'} 
                     className={`${style['mode-box']} ${style['known-writing']}`}
-                    onClick={() => router.push('./test/known_writing')}
                     >
                     <h2 className={alegreya.className}>По написанию</h2>
                     <p className={`${style['known-value']} ${shippori_mincho.className}`}>学</p>
                     <p className={style['checking']}>Проверяется знание<br/><i>чтений, значений</i></p>
-                </div>
+                </Link>
                 <div className={`${style['mode-box']} ${style['known-reading']}`}>
                     <h2 className={alegreya.className}>По чтениям</h2>
                     <p className={`${style['known-value']} ${shippori_mincho.className}`}>まなぶ,<br/>がく</p>
