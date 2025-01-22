86c1c468e26e75b7bef507772e800dce70a03ebc
18degrees <mikhaili.dev@gmail.com>
2025-01-22 09:02:40 +0300

    design: change front page appearance

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import style from './page.module.css'
 import { alegreya, shippori_mincho } from './fonts'
 import Link from 'next/link'
 import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 
 export default async function Page() {
     const session = await getServerSession()
@@ -9,6 +10,53 @@ export default async function Page() {
     return (
         <div className={style.container}>
             <div>
+                <div className={style['first-view']}>
+                    <div className={alegreya.className}>
+                        <Image
+                            src='/big-rectangle-logo.png'
+                            alt='Furoku логотип'
+                            width={270}
+                            height={270}
+                            priority
+                            quality={100}
+                        />
+                        <p>Furoku</p>
+                        <p className={shippori_mincho.className}>付録</p>
+                        <h1>Инструмент для изучения иероглифов</h1>
+                    </div>
+                </div>
+                <div>
+                    <section>
+                        <h2 className={alegreya.className}>Идея</h2>
+                        <p> 
+                            Стандартные способы повторения кандзи эффективны, но не так точны: порядок попадающихся иероглифов напрямую не зависит от потребности в повторении: часто встречаешь те, что уже усвоены; 
+                            не учитывается 
+                        </p>
+                        <ul>
+                            <li>Насколько хорошо иероглиф вспоминался раньше?</li>
+                            <li>Как давно иероглиф был перед глазами?</li>
+                            <li>Как часто используется иероглиф?</li>
+                        </ul>
+                        <p>
+                            Опираясь на ответы, furoku <strong>сортирует кандзи (и их сочетания) по срочности повторения</strong>.
+                        </p>
+                    </section>
+                    <section>
+                        <h2 className={alegreya.className}>Как использовать сайт</h2>
+                        <ol>
+                            <li>Чтобы сохранять иероглифы и сочетания, которые хотите повторять, зарегистрируйтесь.</li>
+                            <li>Посмотрите в списке иероглифов, какие кандзи или сочетания хотите запомнить.</li>
+                            <li>Для повторения перейдите на одноимённую страницу.</li>
+                            <li>Периодически проходите тестирование, чтобы на странице повторения были актуальные результаты.</li>
+                        </ol>
+                    </section>
+                    <section>
+                        <h2 className={alegreya.className}>Обратная связь</h2>
+                        <p>Если есть желание, по адресу ниже можете поделиться любым пользовательским опытом, в т. ч. идеями, жалобами, предложениями.</p>
+                    </section>
+                </div>
+            </div>
+            {/* <div>
                 <div className={style["header-block"]}>
                     <span className={shippori_mincho.className}>付録</span>
                     <h1 className={alegreya.className}>furoku&#160;- инструмент для&#160;изучения иероглифов</h1>
@@ -144,7 +192,7 @@ export default async function Page() {
                         </p>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
