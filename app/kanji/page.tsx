83676f74f418f0f9c25544c93476d31b7d299402
18degrees <mikhaili.dev@gmail.com>
2025-01-29 10:26:11 +0300

    feat: add route and info page for kanji section

---
+++ app/kanji/page.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { ibm_plex_sans_jp } from '../fonts'
+import style from './page.module.css'
+
+export default function Page() {
+    return (
+        <div className={style.container}>
+            <Link href='/kanji/single'>
+                <label>К списку<br/> иероглифов</label>
+                <div className={`${style.preview} ${style.single} ${ibm_plex_sans_jp.className}`}>
+                    <p>人 自</p>
+                    <p>最 近</p>
+                    <p>録 仕</p>
+                </div>
+            </Link>
+            <Link href='/kanji/combo'>
+                <label>К списку<br/> сочетаний<br/> иероглифов</label>
+                <div className={`${style.preview} ${style.combo} ${ibm_plex_sans_jp.className}`}>
+                    <p>従業員</p>
+                    <p>事業</p>
+                </div>
+            </Link>
+            <Link href='/kanji/info'>
+                <label>О списках</label>
+                <div className={`${style.preview} ${style.info}`}>
+                    <p>Содержимое карточек</p>
+                    <p>Упрощение поиска</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
