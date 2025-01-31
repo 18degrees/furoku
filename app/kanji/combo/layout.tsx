ce9205ee3fad780a6a49f5475df483c0e09a6f53
18degrees <mikhaili.dev@gmail.com>
2025-01-31 22:41:20 +0300

    seo: clarify /kanji/combo description metatag

--- app/kanji/combo/layout.tsx
+++ app/kanji/combo/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'Сочетания кандзи по порядку',
-    description: 'Иероглифы, отфильтрованные по jlpt или классу прохождения и отсортированные по частоте употребления или количеству черт',
+    description: 'Сочетания иероглифов, отфильтрованные по jlpt или классу прохождения и отсортированные по частоте употребления на вики',
     alternates: {
         canonical: '/kanji/combo'
     }
