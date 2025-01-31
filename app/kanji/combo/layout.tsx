c43b9cc94d2862426088e3d7355024a2483e23f4
18degrees <mikhaili.dev@gmail.com>
2025-01-31 22:38:11 +0300

    seo: provide canonical metatag for kanji pages

--- app/kanji/combo/layout.tsx
+++ app/kanji/combo/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'Сочетания кандзи по порядку',
-    description: 'Иероглифы, отфильтрованные по jlpt или классу прохождения и отсортированные по частоте употребления или количеству черт'
+    description: 'Иероглифы, отфильтрованные по jlpt или классу прохождения и отсортированные по частоте употребления или количеству черт',
+    alternates: {
+        canonical: '/kanji/combo'
+    }
 }
 
 export default function Layout({children}: {children: React.ReactNode}) {
--- app/kanji/single/layout.tsx
+++ app/kanji/single/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'Кандзи по порядку',
-    description: 'Иероглифы, отфильтрованные по jlpt или классу прохождения и отсортированные по частоте употребления или количеству черт'
+    description: 'Иероглифы, отфильтрованные по jlpt или классу прохождения и отсортированные по частоте употребления или количеству черт',
+    alternates: {
+        canonical: '/kanji/single'
+    }
 }
 
 export default function Layout({children}: {children: React.ReactNode}) {
