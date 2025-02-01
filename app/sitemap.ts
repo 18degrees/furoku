7672aa50da88021024590f19dabe580b97b86e1c
18degrees <mikhaili.dev@gmail.com>
2025-02-01 10:37:24 +0300

    seo: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,10 +9,28 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://furoku.ru/kanji/single',
+      url: 'https://furoku.ru/kanji/info',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
-    }
+    },
+    {
+      url: 'https://furoku.ru/test/info',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://furoku.ru/kanji/single',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://furoku.ru/kanji/combo',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
   ]
 }
