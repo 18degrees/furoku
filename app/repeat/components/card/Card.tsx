421a5eb157beffb5881e2645435ebc6dcd979ca7
18degrees <mikhaili.dev@gmail.com>
2025-02-15 01:52:44 +0300

    design: rework cards view on repeat page

--- app/repeat/components/card/Card.tsx
+++ app/repeat/components/card/Card.tsx
@@ -24,64 +24,67 @@ export default function Card({cardInfo, knownValue, removeCard, increaseCardPoin
     }, [knownValue])
     
     return (
-        <div>
-            <div className={style['show-btns']}>
-                <button 
-                    className={isWritingShown ? style.active : ''}
-                    onClick={() => setIsWritingShown((prev) => !prev)}
-                    disabled={!cardInfo.writing}
-                    style={{
-                        display: knownValue === 'writing' ? 'none' : 'block'
-                    }}>{isWritingShown ? 'Скрыть' : 'Показать'} написание
-                </button>
-                <button 
-                    className={isReadingShown ? style.active : ''}
-                    onClick={() => setIsReadingShown((prev) => !prev)}
-                    disabled={!cardInfo.readings[0]}
-                    style={{
-                        display: knownValue === 'reading' ? 'none' : 'block'
-                    }}>{isReadingShown ? 'Скрыть' : 'Показать'} чтения
-                </button>
-                <button 
-                    className={isMeaningShown ? style.active : ''}
-                    onClick={() => setIsMeaningShown((prev) => !prev)}
-                    disabled={!cardInfo.meanings[0]}
-                    style={{
-                        display: knownValue === 'meaning' ? 'none' : 'block'
-                    }}>{isMeaningShown ? 'Скрыть' : 'Показать'} значения
-                </button>
-            </div>
-            <div className={style.card}>
+        <div className={style['card-container']}>
+            <div className={`${style.card} ${cardInfo.writing.length === 1 ? style.single : style.combo}`}>
                 <button 
                     className={style.remove}
                     onClick={() => removeCard(cardInfo.id)}
-                    ><RemoveIcon/></button>
-                <div 
-                    className={`${shippori_mincho.className} ${style.writing}`}
-                    style={{
-                        display: knownValue === 'writing' || isWritingShown ? 'block' : 'none'
-                    }}
-                    >
-                    <p>{cardInfo.writing}</p>
-                </div>
-                <div
-                    className={ibm_plex_sans_jp.className}
-                    style={{
-                        display: knownValue === 'reading' || isReadingShown ? 'block' : 'none'
-                    }}
-                    >
-                    <ul>
-                        {cardInfo.readings[0] ? cardInfo.readings.map(reading => <li key={reading}>{reading}</li>) : <li>?</li>}
-                    </ul>
-                </div>
-                <div
-                    style={{
-                        display: knownValue === 'meaning' || isMeaningShown ? 'block' : 'none'
-                    }}
-                    >
-                    <ul>
-                        {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <li key={meaning}>{meaning}</li>) : <li>?</li>}
-                    </ul>
+                    ><RemoveIcon/>
+                </button>
+                <div>
+                    {
+                        knownValue === 'writing' ? <>
+                        <div 
+                            className={`${style.writing} ${isWritingShown ? style.active : ''} ${shippori_mincho.className}`}
+                            >
+                            <p>{cardInfo.writing}</p>
+                        </div>
+                        <div className={`${style.reading} ${isReadingShown ? style.active : ''}`}>
+                            <h2 className={alegreya.className} onClick={() => setIsReadingShown(prev => !prev)}>Чтения <span><SortTriangle/></span></h2>
+                            <ul>
+                                {cardInfo.readings[0] ? cardInfo.readings.map(reading => <li key={reading}>{reading}</li>) : <li>?</li>}
+                            </ul>
+                        </div>
+                        <div className={`${style.meaning} ${isMeaningShown ? style.active : ''}`}>
+                            <h2 className={alegreya.className} onClick={() => setIsMeaningShown(prev => !prev)}>Значения <span><SortTriangle/></span></h2>
+                            <ul>
+                                {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <li key={meaning}>{meaning}</li>) : <i>Неизвестны</i>}
+                            </ul>
+                        </div>
+                        </> : knownValue === 'reading' ? <>
+                        <div className={`${style.reading} ${isReadingShown ? style.active : ''}`}>
+                            <ul>
+                                {cardInfo.readings[0] ? cardInfo.readings.map(reading => <li key={reading}>{reading}</li>) : <li>?</li>}
+                            </ul>
+                        </div>
+                        <div className={`${style.writing} ${isWritingShown ? style.active : ''} ${shippori_mincho.className}`}>
+                            <h2 className={alegreya.className} onClick={() => setIsWritingShown(prev => !prev)}>Написание <span><SortTriangle/></span></h2>
+                            <div><p>{cardInfo.writing}</p></div>
+                        </div>
+                        <div className={`${style.meaning} ${isMeaningShown ? style.active : ''}`}>
+                            <h2 className={alegreya.className} onClick={() => setIsMeaningShown(prev => !prev)}>Значения <span><SortTriangle/></span></h2>
+                            <ul>
+                                {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <li key={meaning}>{meaning}</li>) : <i>Неизвестны</i>}
+                            </ul>
+                        </div>  
+                        </> : <>
+                        <div className={`${style.meaning} ${isReadingShown ? style.active : ''}`}>
+                            <ul>
+                                {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <li key={meaning}>{meaning}</li>) : <i>Неизвестны</i>}
+                            </ul>
+                        </div>
+                        <div className={`${style.reading} ${isReadingShown ? style.active : ''}`}>
+                            <h2 className={alegreya.className} onClick={() => setIsReadingShown(prev => !prev)}>Чтения <span><SortTriangle/></span></h2>
+                            <ul>
+                                {cardInfo.readings[0] ? cardInfo.readings.map(reading => <li key={reading}>{reading}</li>) : <li>?</li>}
+                            </ul>
+                        </div>
+                        <div className={`${style.writing} ${isWritingShown ? style.active : ''} ${shippori_mincho.className}`}>
+                            <h2 className={alegreya.className} onClick={() => setIsWritingShown(prev => !prev)}>Написание <span><SortTriangle/></span></h2>
+                            <div><p>{cardInfo.writing}</p></div>
+                        </div>
+                        </>
+                    }
                 </div>
                 <button 
                     className={`${style.increase} ${alegreya_sans.className}`}
@@ -89,56 +92,123 @@ export default function Card({cardInfo, knownValue, removeCard, increaseCardPoin
                     >+1
                 </button>
             </div>
+            
         </div>
+        // <div>
+        //     <div className={style['show-btns']}>
+        //         <button 
+        //             className={isWritingShown ? style.active : ''}
+        //             onClick={() => setIsWritingShown((prev) => !prev)}
+        //             disabled={!cardInfo.writing}
+        //             style={{
+        //                 display: knownValue === 'writing' ? 'none' : 'block'
+        //             }}>{isWritingShown ? 'Скрыть' : 'Показать'} написание
+        //         </button>
+        //         <button 
+        //             className={isReadingShown ? style.active : ''}
+        //             onClick={() => setIsReadingShown((prev) => !prev)}
+        //             disabled={!cardInfo.readings[0]}
+        //             style={{
+        //                 display: knownValue === 'reading' ? 'none' : 'block'
+        //             }}>{isReadingShown ? 'Скрыть' : 'Показать'} чтения
+        //         </button>
+        //         <button 
+        //             className={isMeaningShown ? style.active : ''}
+        //             onClick={() => setIsMeaningShown((prev) => !prev)}
+        //             disabled={!cardInfo.meanings[0]}
+        //             style={{
+        //                 display: knownValue === 'meaning' ? 'none' : 'block'
+        //             }}>{isMeaningShown ? 'Скрыть' : 'Показать'} значения
+        //         </button>
+        //     </div>
+        //     <div className={style.card}>
+        //         <button 
+        //             className={style.remove}
+        //             onClick={() => removeCard(cardInfo.id)}
+        //             ><RemoveIcon/></button>
+        //         <div 
+        //             className={`${shippori_mincho.className} ${style.writing}`}
+        //             style={{
+        //                 display: knownValue === 'writing' || isWritingShown ? 'block' : 'none'
+        //             }}
+        //             >
+        //             <p>{cardInfo.writing}</p>
+        //         </div>
+        //         <div
+        //             className={ibm_plex_sans_jp.className}
+        //             style={{
+        //                 display: knownValue === 'reading' || isReadingShown ? 'block' : 'none'
+        //             }}
+        //             >
+        //             <ul>
+        //                 {cardInfo.readings[0] ? cardInfo.readings.map(reading => <li key={reading}>{reading}</li>) : <li>?</li>}
+        //             </ul>
+        //         </div>
+        //         <div
+        //             style={{
+        //                 display: knownValue === 'meaning' || isMeaningShown ? 'block' : 'none'
+        //             }}
+        //             >
+        //             <ul>
+        //                 {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <li key={meaning}>{meaning}</li>) : <li>?</li>}
+        //             </ul>
+        //         </div>
+        //         <button 
+        //             className={`${style.increase} ${alegreya_sans.className}`}
+        //             onClick={() => increaseCardPoints(cardInfo.id)}
+        //             >+1
+        //         </button>
+        //     </div>
+        // </div>
         // <div className={style.card}>
-        //     <button onClick={() => removeCard(kanjiInfo.writing)}><RemoveIcon/></button>
+        //     <button onClick={() => removeCard(cardInfo.writing)}><RemoveIcon/></button>
         //     {knownValue === 'writing' ? <>
-        //         <div className={`${style.writing} ${isWritingShown ? style.active : ''} ${shippori_mincho.className}`}><p>{kanjiInfo.writing}</p></div>
+        //         <div className={`${style.writing} ${isWritingShown ? style.active : ''} ${shippori_mincho.className}`}><p>{cardInfo.writing}</p></div>
         //         <div className={`${style.reading} ${isReadingShown ? style.active : ''}`}>
         //             <h2 className={alegreya.className} onClick={() => setIsReadingShown(prev => !prev)}>Чтения <span><SortTriangle/></span></h2>
         //             <div>
-        //                 <div>{kanjiInfo.kun_readings[0] ? <p className={ibm_plex_sans_jp.className}>{kanjiInfo.kun_readings.join(' ')}</p> : <i>Кунные неизвестны</i>}</div>
+        //                 <div>{cardInfo.kun_readings[0] ? <p className={ibm_plex_sans_jp.className}>{cardInfo.kun_readings.join(' ')}</p> : <i>Кунные неизвестны</i>}</div>
         //                 <hr/>
-        //                 <div>{kanjiInfo.on_readings[0] ? <p className={ibm_plex_sans_jp.className}>{kanjiInfo.on_readings.join(' ')}</p> : <i>Онные неизвестны</i>}</div>
+        //                 <div>{cardInfo.on_readings[0] ? <p className={ibm_plex_sans_jp.className}>{cardInfo.on_readings.join(' ')}</p> : <i>Онные неизвестны</i>}</div>
         //             </div>
         //         </div>
         //         <div className={`${style.meaning} ${isMeaningShown ? style.active : ''}`}>
         //             <h2 className={alegreya.className} onClick={() => setIsMeaningShown(prev => !prev)}>Значения <span><SortTriangle/></span></h2>
         //             <div>
-        //                 {kanjiInfo.meanings[0] ? kanjiInfo.meanings.map(meaning => <p key={meaning}>{meaning}</p>) : <i>Неизвестны</i>}
+        //                 {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <p key={meaning}>{meaning}</p>) : <i>Неизвестны</i>}
         //             </div>
         //         </div>
         //     </> : knownValue === 'reading' ? <>
         //         <div className={`${style.reading} ${isReadingShown ? style.active : ''}`}>
-        //             <div>{kanjiInfo.kun_readings[0] ? <p className={ibm_plex_sans_jp.className}>{kanjiInfo.kun_readings.join(' ')}</p> : <i>Кунные неизвестны</i>}</div>
+        //             <div>{cardInfo.kun_readings[0] ? <p className={ibm_plex_sans_jp.className}>{cardInfo.kun_readings.join(' ')}</p> : <i>Кунные неизвестны</i>}</div>
         //             <hr/>
-        //             <div>{kanjiInfo.on_readings[0] ? <p className={ibm_plex_sans_jp.className}>{kanjiInfo.on_readings.join(' ')}</p> : <i>Онные неизвестны</i>}</div>
+        //             <div>{cardInfo.on_readings[0] ? <p className={ibm_plex_sans_jp.className}>{cardInfo.on_readings.join(' ')}</p> : <i>Онные неизвестны</i>}</div>
         //         </div>
         //         <div className={`${style.writing} ${isWritingShown ? style.active : ''} ${shippori_mincho.className}`}>
         //             <h2 className={alegreya.className} onClick={() => setIsWritingShown(prev => !prev)}>Написание <span><SortTriangle/></span></h2>
-        //             <div><p>{kanjiInfo.writing}</p></div>
+        //             <div><p>{cardInfo.writing}</p></div>
         //         </div>
         //         <div className={`${style.meaning} ${isMeaningShown ? style.active : ''}`}>
         //             <h2 className={alegreya.className} onClick={() => setIsMeaningShown(prev => !prev)}>Значения <span><SortTriangle/></span></h2>
         //             <div>
-        //                 {kanjiInfo.meanings[0] ? kanjiInfo.meanings.map(meaning => <p key={meaning}>{meaning}</p>) : <i>Неизвестны</i>}
+        //                 {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <p key={meaning}>{meaning}</p>) : <i>Неизвестны</i>}
         //             </div>
         //         </div>  
         //     </> : <>
         //             <div>
-        //                 {kanjiInfo.meanings[0] ? kanjiInfo.meanings.map(meaning => <p key={meaning}>{meaning}</p>) : <i>Неизвестны</i>}
+        //                 {cardInfo.meanings[0] ? cardInfo.meanings.map(meaning => <p key={meaning}>{meaning}</p>) : <i>Неизвестны</i>}
         //             </div>
         //         <div className={`${style.reading} ${isReadingShown ? style.active : ''}`}>
         //             <h2 className={alegreya.className} onClick={() => setIsReadingShown(prev => !prev)}>Чтения <span><SortTriangle/></span></h2>
         //             <div>
-        //                 <div>{kanjiInfo.kun_readings[0] ? <p className={ibm_plex_sans_jp.className}>{kanjiInfo.kun_readings.join(' ')}</p> : <i>Кунные неизвестны</i>}</div>
+        //                 <div>{cardInfo.kun_readings[0] ? <p className={ibm_plex_sans_jp.className}>{cardInfo.kun_readings.join(' ')}</p> : <i>Кунные неизвестны</i>}</div>
         //                 <hr/>
-        //                 <div>{kanjiInfo.on_readings[0] ? <p className={ibm_plex_sans_jp.className}>{kanjiInfo.on_readings.join(' ')}</p> : <i>Онные неизвестны</i>}</div>
+        //                 <div>{cardInfo.on_readings[0] ? <p className={ibm_plex_sans_jp.className}>{cardInfo.on_readings.join(' ')}</p> : <i>Онные неизвестны</i>}</div>
         //             </div>
         //         </div>
         //         <div className={`${style.writing} ${isWritingShown ? style.active : ''} ${shippori_mincho.className}`}>
         //             <h2 className={alegreya.className} onClick={() => setIsWritingShown(prev => !prev)}>Написание <span><SortTriangle/></span></h2>
-        //             <div><p>{kanjiInfo.writing}</p></div>
+        //             <div><p>{cardInfo.writing}</p></div>
         //         </div>
         //     </>}
         // </div>
--- app/repeat/page.tsx
+++ app/repeat/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useCallback, useContext } from "react"
 import { AlertsContext } from "../components/providers/Alerts/AlertsProvider"
 import { IDefaultBodyResponse } from "../interfaces/response.interface"
 import { knownValue, pointsOf } from "../interfaces/kanji.interface"
+import type { TouchEvent, MouseEvent } from 'react'
 import { SortTriangle } from "./components/Sort-triangle-icon"
 import { IKanjiProps } from "../interfaces/kanji.interface"
 import { Spinner } from "../components/spinner/Spinner"
@@ -21,13 +22,19 @@ interface IResponseBody extends IDefaultBodyResponse {
 type ShowType = 'single' | 'combo'
 type Path = `/api/profile/kanji/single` | `/api/profile/kanji/combo`
 
+type menu = 'showType' | 'knownValue' | 'pointsOf'
+
+const PART_OF_SCREEN_TO_SWIPE = 0.2
+
 let isFirstLoadingProcessing = true
 
 export default function Profile() {
     const {request} = useHttp()
     const [cards, setCards] = useState<IKanjiProps[] | null>(null)
-    const [index, setIndex] = useState(0)
+    const [index, setIndex] = useState(0)                               //индекс первой (самой левой) карточки на экране
     const [hasPointsChanged, setHasPointsChanged] = useState(false)
+
+    const [columnsCount, setColumnsCount] = useState<null | number>(null)
 
     const [isResponseLoading, setIsResponseLoading] = useState(true)
 
@@ -43,6 +50,8 @@ export default function Profile() {
     const [isShowTypenMenuOpened, setIsShowTypenMenuOpened] = useState(false)
     const [isKnownMenuOpened, setIsKnownMenuOpened] = useState(false)
     const [isPointsMenuOpened, setIsPointsnMenuOpened] = useState(false)
+
+    let touchStartLeftCoord = 0;
 
     const getCards = useCallback(async(): Promise<void> => {
         setIsResponseLoading(true)
@@ -157,6 +166,69 @@ export default function Profile() {
         setCards(updCards)
     }
 
+    useEffect(() => {
+        function changeColumnsCount() {
+            const windowWidth = Math.max(
+                document.body.clientWidth,
+                document.documentElement.clientWidth
+            );
+            setColumnsCount(() => windowWidth > 1120 ? 3 : windowWidth > 730 ? 2 : 1)
+        }
+        changeColumnsCount()
+        window.addEventListener('resize', changeColumnsCount)
+    }, [])
+
+    function onIndexDecrement() {
+        if (!columnsCount) return
+
+        setIndex(prev => prev - columnsCount >= 0 ? prev - columnsCount : prev)
+    }
+    function onIndexIncrement() {
+        if (!columnsCount || !cards) return
+
+        setIndex(prev => prev + columnsCount <= cards.length - 1 ? prev + columnsCount : prev)
+    }
+
+    useEffect(() => {
+        setIndex((prev) => {
+            const properIndex = getNearestSmallestDivisibleNumber(prev)
+
+            return properIndex
+        })
+
+        function getNearestSmallestDivisibleNumber(variant: number) {
+            if (!columnsCount || variant === 0) return variant
+
+            if (variant % columnsCount) return getNearestSmallestDivisibleNumber(variant - 1)
+
+            return variant
+        }
+    }, [columnsCount])
+
+    function onMouseDown(mouseX: number) {
+        touchStartLeftCoord = mouseX
+    }
+    function onMouseUp(touchEndLeftCoord: number) {
+        const windowWidth = window.innerWidth
+        
+        if (!cards) return
+        
+        const swipeWidth = touchEndLeftCoord - touchStartLeftCoord
+        
+        const swipeAbsoluteValue = Math.abs(swipeWidth)
+
+        if (swipeAbsoluteValue / windowWidth < PART_OF_SCREEN_TO_SWIPE) return
+
+        const swipeDirection = swipeWidth > 0 ? 'RIGHT' : 'LEFT'
+        
+        switch (swipeDirection) {
+            case 'LEFT':
+                onIndexIncrement()
+                break;    
+            case 'RIGHT':
+                onIndexDecrement()
+        }
+    }
 
     function menuToggler(menuType: menu) {
         switch (menuType) {
@@ -301,15 +373,41 @@ export default function Profile() {
                 </div>
                 {
                     cards ?
-                        cards.map(cardObj => {
-                            return (<Card 
-                                cardInfo={cardObj}
-                                knownValue={knownValue}
-                                key={cardObj.id}
-                                removeCard={removeCard}
-                                increaseCardPoints={increaseCardPoints}
-                            />)
-                        }) : !isResponseLoading ? 
+                        <>
+                            <div className={style['buttons']}>
+                                <button 
+                                    onClick={() => onIndexDecrement()}
+                                    disabled={!columnsCount || !cards || index === 0}
+                                    >Назад
+                                </button>
+                                <button 
+                                    onClick={() => onIndexIncrement()}
+                                    disabled={!columnsCount || !cards || index + columnsCount > cards.length - 1}
+                                    >Вперёд
+                                </button>
+                            </div>
+                            <div 
+                                className={style.cards}
+                                onMouseDown={(event: MouseEvent<HTMLDivElement>) => onMouseDown(event.clientX)}
+                                onMouseUp={(event: MouseEvent<HTMLDivElement>) => onMouseUp(event.clientX)}
+                                onTouchStart={(event: TouchEvent<HTMLDivElement>) => onMouseDown(event.touches[0].clientX)}
+                                onTouchEnd={(event: TouchEvent<HTMLDivElement>) =>onMouseUp(event.changedTouches[0].clientX)}
+                                style={{
+                                    transform: `translateX(${ columnsCount ? Math.floor(index / columnsCount) * -100 : 0 }vw)`
+                                }}
+                                >
+                                {cards.map(cardObj => {
+                                    return (<Card 
+                                        cardInfo={cardObj}
+                                        knownValue={knownValue}
+                                        key={cardObj.id}
+                                        removeCard={removeCard}
+                                        increaseCardPoints={increaseCardPoints}
+                                    />)
+                                })}
+                            </div>
+                        </>
+                         : !isResponseLoading ? 
                         <p className={style['non-kanji-plug']}>
                             Кажется, вы ещё не добавили {showType === 'single' ? 'иероглифы' : 'сочетания'} к повторению.<br/>
                             Посмотрите в <Link href={`/kanji/${showType === 'single' ? 'single' : 'combo'}`}>списке</Link>.
